6169e9ad2c34adf1a722ca58014c7ad7ddc76faf
Cedric Holz <cedricholz@gmail.com>
2022-12-20 15:18:46 -0800

    push

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("SUP")
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(25)
+
+try:
+	while True:
+		servo.min()
+		sleep(0.5)
+		servo.mid()
+		sleep(0.5)
+		servo.max()
+		sleep(0.5)
+		break
+except KeyboardInterrupt:
+	print("Program stopped")
